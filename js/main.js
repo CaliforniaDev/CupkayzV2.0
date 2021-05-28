2a69bece098b92e6b9844305c8383a5d551e5022
Leo <leo@nybblebox.com>
2021-05-28 03:19:06 -0700

    Add mobile nav bar overlay

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 
 window.onload = () => {
     const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector("#nav-menu");
+    const navMenu = document.querySelector("#mobile-nav-menu");
+    const navOverlay = document.querySelector(".nav-overlay");
     const navLink = document.querySelectorAll(".nav-link");
     
     hamburger.addEventListener("click", openMobileNav);
@@ -12,10 +13,12 @@ window.onload = () => {
     function openMobileNav() {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
+        navOverlay.classList.toggle("active");
     }
     function closeMobileMenu() {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
+        navOverlay.classList.toggle("active");
     }
 }
 
