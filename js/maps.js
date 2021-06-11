3a6a62d9d4704aeb8bb5e790e25c99276d9573a5
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-06-11 04:36:33 -0700

    Fix form from submitting after pressing enter key

--- js/maps.js
+++ js/maps.js
@@ -165,6 +165,16 @@ function initMap() {
         map.setCenter(mapOptions.center);
         directionRenderer.setMap(null)
     }
+    $('form input').keydown(function (e) {
+        if (e.keyCode == 13) {
+            var inputs = $(this).parents("form").eq(0).find(":input");
+            if (inputs[inputs.index(this) + 1] != null) {                    
+                inputs[inputs.index(this) + 1].focus();
+            }
+            e.preventDefault();
+            return false;
+        }
+    });
     
 
 
