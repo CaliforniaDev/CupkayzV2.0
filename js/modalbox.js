4684b8965c43c9dae92173ab24b673cdff937d84
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-07-28 04:46:34 -0700

    Refactor variable declaration inside functions

--- js/modalbox.js
+++ js/modalbox.js
@@ -1,46 +1,16 @@
 window.onload = init;
 
 function init() {
-    const PRODUCT_CARD = document.querySelectorAll(".card");
-    const MODAL_CONTAINER = document.querySelector(".modal-container");
-    const closeBtn = document.querySelector(".close-btn");
-    const modalBoxImage = document.querySelector(".modal-box > figure > img");
-    const modalBoxName = document.querySelector(".info-container > h4");
-    const modalBoxDetail = document.querySelector(".info-container > p");
-    const modalBoxPrice = document.querySelector(".total-price");
 
     addClickListener()
 
     function addClickListener() {
-        for(let i = 0; i < PRODUCT_CARD.length; i++) {
-            PRODUCT_CARD[i].addEventListener("click", setAndOpenModalBox);
-            console.log("hello World");
+        let productCard = document.querySelectorAll(".card");
+        let closeBtn = document.querySelector(".close-btn");
+        for(let i = 0; i < productCard.length; i++) {
+            productCard[i].addEventListener("click", setAndOpenModalBox);
         }
         closeBtn.addEventListener("click", closeModalContainer);
-    }
-
-    function setAndOpenModalBox(event) {
-        let item = event.currentTarget;
-        let imageSource = item.querySelector(".item-img").getAttribute("src");
-        let itemName = item.querySelector(".item-name").innerHTML;
-        let itemDetail = item.querySelector(".item-detail").innerHTML;
-        let itemPrice = item.querySelector(".item-price").innerHTML;
-        let modalBoxCard = new ModalBox(imageSource, itemName, itemDetail, itemPrice);
-        setModalBox(modalBoxCard);
-        openModalContainer()
-    }
-    function setModalBox(object) {
-        modalBoxImage.src = object.image;
-        modalBoxName.innerHTML = object.name;
-        modalBoxDetail.innerHTML = object.detail;
-        modalBoxPrice.innerHTML = object.price;
-    }
-
-    function openModalContainer() {
-        MODAL_CONTAINER.classList.toggle("active");
-    }
-    function closeModalContainer() {
-        MODAL_CONTAINER.classList.toggle("active");
     }
 
     function ModalBox(image, name, detail, price) {
@@ -50,7 +20,37 @@ function init() {
         this.price = price;
     }
 
-    // function ModalBox(imgSrc, name, detail, price)
+    function setAndOpenModalBox(event) {
+        let item = event.currentTarget;
+        let imageSource = item.querySelector(".item-img").getAttribute("src");
+        let itemName = item.querySelector(".item-name").innerHTML;
+        let itemDetail = item.querySelector(".item-detail").innerHTML;
+        let itemPrice = item.querySelector(".item-price").innerHTML;
+        let modalBoxCard = new ModalBox(imageSource, itemName, itemDetail, itemPrice);
+        
+        setModalBox(modalBoxCard);
+        openModalContainer()
+    }
+    function setModalBox(object) {
+        let modalBoxImage = document.querySelector(".modal-box > figure > img");
+        let modalBoxName = document.querySelector(".info-container > h4");
+        let modalBoxDetail = document.querySelector(".info-container > p");
+        let modalBoxPrice = document.querySelector(".total-price");
+
+        modalBoxImage.src = object.image;
+        modalBoxName.innerHTML = object.name;
+        modalBoxDetail.innerHTML = object.detail;
+        modalBoxPrice.innerHTML = object.price;
+    }
+
+    function openModalContainer() {
+        const MODAL_CONTAINER = document.querySelector(".modal-container");
+        MODAL_CONTAINER.classList.toggle("active");
+    }
+    function closeModalContainer() {
+        const MODAL_CONTAINER = document.querySelector(".modal-container");
+        MODAL_CONTAINER.classList.toggle("active");
+    }
 
 
 
