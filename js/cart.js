c3da600a343c190cce4222c7fde1164858c270e3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-08-16 16:23:27 -0700

    Add app height function to help with ios safari resize

--- js/cart.js
+++ js/cart.js
@@ -3,3 +3,9 @@ let shoppingCart_deserialized = JSON.parse(localStorage.getItem("cart-array"));
 console.log(shoppingCart_deserialized);
 
 
+const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`)
+}
+window.addEventListener("resize", appHeight);
+appHeight()
