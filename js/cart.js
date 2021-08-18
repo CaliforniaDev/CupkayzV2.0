eb3ac70a101a9618d794e24b3780b28e1020ba7a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-08-18 04:39:47 -0700

    Add iife

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,15 @@
-document.querySelector(".delivery-address").innerHTML = localStorage.getItem("recipient-address");
-console.log(localStorage);
-let shoppingCart_deserialized = JSON.parse(localStorage.getItem("cart-array"));
-console.log(shoppingCart_deserialized);
+(function () {
+    document.querySelector(".delivery-address").innerHTML = localStorage.getItem("recipient-address");
+    console.log(localStorage);
+    let shoppingCart_deserialized = JSON.parse(localStorage.getItem("cart-array"));
+    console.log(shoppingCart_deserialized);
 
 
-const appHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty("--app-height", `${window.innerHeight}px`)
-}
-window.addEventListener("resize", appHeight);
-appHeight()
+    const appHeight = () => {
+        const doc = document.documentElement
+        doc.style.setProperty("--app-height", `${window.innerHeight}px`)
+    }
+    window.addEventListener("resize", appHeight);
+    appHeight()
+
+}());
