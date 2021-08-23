3efbc9c316203335c1dbc75d8ab2b590cd26ea31
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-08-23 00:19:06 -0700

    Add remove item function to DOM

--- js/cart.js
+++ js/cart.js
@@ -12,4 +12,22 @@
     window.addEventListener("resize", appHeight);
     appHeight()
 
+    
+    addClickEventListener();
+
+    function addClickEventListener() {
+        let removeCartItemButton = document.querySelectorAll(".remove-btn");
+        for(let i = 0; i < addEventListener.length; i++) {
+            let button = removeCartItemButton[i];
+            button.addEventListener("click", removeItem);
+            console.log(button);
+        }
+    }
+
+    function removeItem(event) {
+        let buttonClicked = event.currentTarget;
+        buttonClicked.parentElement.remove();
+    }
+    
+
 }());
