f0ed5567e3133ecf019c6ff1d3036ef8fede825d
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-08-26 04:55:25 -0700

    Add subtract button functions

--- js/custom-select-btn.js
+++ js/custom-select-btn.js
@@ -11,19 +11,30 @@
     const PLUS_BUTTON = document.querySelectorAll(".plus-btn");
     for (let i = 0; i < MINUS_BUTTON.length; i++) {
       MINUS_BUTTON[i].setAttribute("disabled", "disabled");
-      PLUS_BUTTON[i].addEventListener("click", addQuantity);
+      MINUS_BUTTON[i].addEventListener("click", subtractQuantityField)
+      PLUS_BUTTON[i].addEventListener("click", incrementQuantity);
     }
     
   }
 
-  function addQuantity(event) {
+  function incrementQuantity(event) {
     const plusButton = event.currentTarget;
     const inputField = plusButton.previousElementSibling;
+    const minusButton = inputField.previousElementSibling;
+    inputField.value > 1 ? minusButton.removeAttribute("disabled") : false; 
     inputField.value == 100 ? plusButton.setAttribute("disabled", "disabled") : inputField.value++;
     plusButton.getAttribute("disabled") ? alert("reached max quantity") : false;
+   
 
   }
 
+  function subtractQuantityField(event) {
+    const minusButton = event.currentTarget;
+    const inputField = minusButton.nextElementSibling;
+    console.log
+    inputField.value == 1 ? minusButton.setAttribute("disabled", "disabled") : inputField.value--;
+    minusButton.getAttribute("disabled") ? alert("please enter valid amount") : false;
+  } 
 
 
 
