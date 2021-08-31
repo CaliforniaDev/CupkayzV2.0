6c86a565dfe90634fecf303f6ea7fff5c745f956
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2021-08-31 04:47:17 -0700

    Add function that grabs from local storage and place items into cart for display

--- js/cart.js
+++ js/cart.js
@@ -6,19 +6,21 @@
     }
 
     document.querySelector(".delivery-address").innerHTML = localStorage.getItem("recipient-address");
-    const CART_DESERIALIZED = JSON.parse(localStorage.getItem("cart-array"));
-    console.log(CART_DESERIALIZED);
 
     function documentIsReady() {
-        
+        const CART_DESERIALIZED = JSON.parse(localStorage.getItem("cart-array"));
+        const cartItemDiv = document.querySelector(".cart-item");
+    
 
 
-
-
-
-
-
-
+        for (let i = 0; i < CART_DESERIALIZED.length; i++) {
+            const cartItem = CART_DESERIALIZED[i];
+            const title = cartItem.itemName;
+            const imageSrc = cartItem.image;
+            const price = "$" + cartItem.price + " per dozen";
+            const quantity = cartItem.quantity;
+            loadItemsToCart(title, imageSrc, price, quantity);
+        }
         if (isOrderEmpty()) {
             updateCartTotal();
             return alert("cart is empty");
@@ -30,8 +32,18 @@
         }
     }
 
-    function addCartItems() {
-
+    function loadItemsToCart(title, imageSrc, price, quantity) {
+        const cartItem = document.querySelector(".cart-item");
+        const cartReceipt = document.querySelector(".cart-receipt-block");
+        const cartItemClone = cartItem.cloneNode(true);
+        cartItemClone.querySelector(".cart-item__img").src = imageSrc;
+        cartItemClone.querySelector(".cart-item__title").innerText = title;
+        cartItemClone.querySelector(".cart-item__price").innerText = price;
+        cartItemClone.querySelector(".quantity-field").value = quantity;
+        cartItem.classList.add("active");
+        cartReceipt.classList.add("active");
+        console.log(cartItemClone.classList)
+        cartItem.after(cartItemClone);
     }
     function addQuantityInputListeners() {
         const QUANTITY_FIELD = document.querySelectorAll(".quantity-field");
@@ -52,11 +64,11 @@
         if (isNaN(textField.value) || textField.value <= 0) {
             textField.value = 1;
         } else if (textField.value > 100) {
-            textField.value = 100; 
+            textField.value = 100;
         }
         updateCartTotal();
     }
-    
+
     function addRemoveButtonListeners() {
         const REMOVE_CART_ITEM_BUTTONS_ARR = document.querySelectorAll(".cart-item__remove-btn");
         for (let i = 0; i < REMOVE_CART_ITEM_BUTTONS_ARR.length; i++) {
@@ -76,7 +88,7 @@
     function updateReviewTitle() {
         let totalItems = 0;
         const orderReviewTitle = document.querySelector(".order-review__title");
-        const orderItems = document.querySelectorAll(".cart-item");
+        const orderItems = document.querySelectorAll(".cart-item.active");
         for (let i = 0; i < orderItems.length; i++) {
             totalItems++;
         }
@@ -86,6 +98,8 @@
 
     function updateCartTotal() {
         let counter = 0;
+        const cartItems = document.querySelectorAll(".cart-item.active");
+        console.log(cartItems);
         const itemPriceArr = document.querySelectorAll(".cart-item__price");
         const quantityArray = document.querySelectorAll(".quantity-field");
         const subtotalContainer = document.querySelector(".cart-subtotal__price");
